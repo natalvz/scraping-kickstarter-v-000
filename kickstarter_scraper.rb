53444cdc0c2466787fc6983791c8bfa2925cc450
Natalia Castro <nat.alvz@gmail.com>
2018-06-13 23:13:13 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,5 +13,5 @@ create_project_hash
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta u li span.location-name").text .gsub("%", "").to_i
+# location: project.css("ul.project-meta span.location-name").text 
 #ruby kickstarter_scraper.rb
