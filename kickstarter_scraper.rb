9b4435418350627eae1129def6392dfeeb613349
Natalia Castro <nat.alvz@gmail.com>
2018-06-13 23:14:40 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,15 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
  
   kickstarter = Nokogiri::HTML(html)
+  
+  projects = {}
+ 
+  # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end 
+  
+  projects 
 
 end
 # projects: kickstarter.css("li.project.grid_4")
